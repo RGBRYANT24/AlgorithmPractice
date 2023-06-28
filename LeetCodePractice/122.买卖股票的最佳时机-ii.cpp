aa15bc248c48d0a5d8bfdd1d7406a08fd6d3384c
ADRIN <1021741772@qq.com>
2023-06-28 11:35:46 +0800

    dp的做法

--- LeetCodePractice/122.买卖股票的最佳时机-ii.cpp
+++ LeetCodePractice/122.买卖股票的最佳时机-ii.cpp
@@ -62,29 +62,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int start = 0;
-        int end = 0;
-        int ans = 0;
-        if(prices.size() <= 1)
+        int n = prices.size();
+        int dp[n + 5][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i < n; i ++)
         {
-            return 0;
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
         }
-        for(int i = 1; i < prices.size(); i++)
-        {
-            if(prices[i] < prices[i - 1])
-            {
-                //end = i;
-                ans += prices[end] - prices[start];
-                start = i;
-                end = i;
-            }
-            else
-            {
-                end = i;
-            }
-        }
-        ans += prices[end] - prices[start];
-        return ans;
+        return max(dp[n - 1][0], dp[n - 1][1]);
     }
 };
 // @lc code=end
