7592308477617b7efd19faaabf45f6a46e7f1092
ADRIN <1021741772@qq.com>
2023-09-04 15:24:32 +0800

    迭代的方法

---
+++ LeetCodePractice/106.从中序与后序遍历序列构造二叉树.cpp
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=106 lang=cpp
+ *
+ * [106] 从中序与后序遍历序列构造二叉树
+ *
+ * https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+ *
+ * algorithms
+ * Medium (71.97%)
+ * Likes:    1086
+ * Dislikes: 0
+ * Total Accepted:    305.1K
+ * Total Submissions: 423.9K
+ * Testcase Example:  '[9,3,15,20,7]\n[9,15,7,20,3]'
+ *
+ * 给定两个整数数组 inorder 和 postorder ，其中 inorder 是二叉树的中序遍历， postorder
+ * 是同一棵树的后序遍历，请你构造并返回这颗 二叉树 。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入：inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+ * 输出：[3,9,20,null,null,15,7]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入：inorder = [-1], postorder = [-1]
+ * 输出：[-1]
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 <= inorder.length <= 3000
+ * postorder.length == inorder.length
+ * -3000 <= inorder[i], postorder[i] <= 3000
+ * inorder 和 postorder 都由 不同 的值组成
+ * postorder 中每一个值都在 inorder 中
+ * inorder 保证是树的中序遍历
+ * postorder 保证是树的后序遍历
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if(!postorder.size()) return nullptr;
+        reverse(inorder.begin(), inorder.end());
+        reverse(postorder.begin(), postorder.end());
+        int index = 0;
+        stack<TreeNode*> stk;
+        TreeNode *root = new TreeNode(postorder[0]);
+        stk.push(root);
+        for(int i = 1; i < postorder.size(); i ++)
+        {
+            TreeNode *node = stk.top();
+            if(stk.top() -> val != inorder[index])
+            {
+                node -> right = new TreeNode(postorder[i]);
+                stk.push(node -> right);
+            }
+            else
+            {
+                while(!stk.empty() && stk.top() -> val == inorder[index])
+                {
+                    node = stk.top();
+                    stk.pop();
+                    index ++;
+                }
+                node -> left = new TreeNode(postorder[i]);
+                stk.push(node -> left);
+            }
+        }
+        return root;
+    }
+};
+// @lc code=end
+
