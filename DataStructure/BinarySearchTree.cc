d62229a94f12a2818dd31bd0bd0207d436c068c3
RGBRYANT24 <1021741772@qq.com>
2023-10-19 22:49:35 +0800

    二叉搜索树构造和中序遍历（递归）

---
+++ DataStructure/BinarySearchTree.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int key;
+    TreeNode* left;
+    TreeNode* right;
+    int size; //当前节点为根的子树大小
+    int count; //当前节点的重复数量
+    //TreeNode(): val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x): key(x), left(nullptr), right(nullptr), size(1){}
+};
+
+TreeNode* insert(TreeNode* root, int val)
+{
+    if(root == nullptr) return new TreeNode(val);
+    if(root -> key < val) root -> left = insert(root -> left, val);
+    if(root -> key > val) root -> right = insert(root -> right, val);
+    else root -> count ++;
+    return root;
+}
+
+//遍历二叉搜索树 递归
+void inorderTraversal(TreeNode* root)
+{
+    if(root == nullptr) return;
+    inorderTraversal(root -> left);
+    cout << root -> key << endl;
+    inorderTraversal(root -> right);
+}
+
+int main()
+{
+    vector<int> nums = {2, 1, 4};
+    //TreeNode* root;
+    /*for(int i = 0; i < nums.size(); i ++)
+    {
+        root = insert(root, nums[i]);
+    }
+    inorderTraversal(root);*/
+    system("pause");
+    return 0;
+}
