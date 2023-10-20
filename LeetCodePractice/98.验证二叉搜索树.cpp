0355e09d9a21b5c60af285899dfef5f80f985ad5
RGBRYANT24 <1021741772@qq.com>
2023-10-20 16:37:50 +0800

    中序遍历

--- LeetCodePractice/98.验证二叉搜索树.cpp
+++ LeetCodePractice/98.验证二叉搜索树.cpp
@@ -19,54 +19,32 @@
 class Solution
 {
 public:
-    /*int count = 0;
-    bool dfs(TreeNode *root,int min, int max)
-    {
-        count++;
-        if(root==nullptr) return true;
-        if(count != 1)//不是根节点
-        {
-            if (root->val <= min)
-                return false;
-            if (root->val >= max)
-            {
-                return false;
-            }
-        }
-        
-        return dfs(root->left,min,root->val) && dfs(root->right,root->val,max);
-        这前面是用上界和下届判断二叉搜索树的，但是样例总卡INT_MAX和IN_MIN      
-    }  
-    */
-    TreeNode *prev;
-    bool isBTS(TreeNode *root)
-    {
-        if(root!=nullptr)
-        {
-            if(!isBTS(root->left))
-            {
-                return false;
-            }
-            if(prev!=nullptr && root->val <= prev->val)
-            {
-                return false;
-            }
-            prev = root;
-            if(!isBTS(root->right))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
     bool isValidBST(TreeNode *root)
     {
-        //int INT_MAX=1;
-        //if (dfs(root,INT_MIN,INT_MAX))
-        if(isBTS(root))
-            return true;
-        else
-            return false;
+         if(!root) return false;
+        int pre = 0;
+        int count = 0;
+        TreeNode* p = root;
+        stack<TreeNode*> stk;
+        while(p || !stk.empty())
+        {
+            while(p)
+            {
+                stk.push(p);
+                p = p -> left;
+            }
+            if(!stk.empty())
+            {
+                p = stk.top();
+                //cout << p -> val << " " << pre << endl;
+                if(count && p -> val <= pre) return false;
+                count ++;
+                pre = p -> val;
+                stk.pop();
+            }
+            p = p -> right;
+        }
+        return true;
     }
 };
 // @lc code=end
