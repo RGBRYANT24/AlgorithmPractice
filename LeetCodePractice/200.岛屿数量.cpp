847f065b0926c2a2c677a4ec5aeba8f0defa47af
ADRIN <1021741772@qq.com>
2023-10-31 23:09:12 +0800

    并查集的做法

--- LeetCodePractice/200.岛屿数量.cpp
+++ LeetCodePractice/200.岛屿数量.cpp
@@ -59,54 +59,87 @@
  */
 
 // @lc code=start
-class Solution {
-public:
-    int numIslands(vector<vector<char>>& grid) {
-        set<pair<int, int>> vis;
-        int n = grid.size(), m = grid[0].size();
-        int ans = 0;
-        for(int i = 0; i < n; i ++)
+class UnionFind{
+private:
+    int count;
+    vector<int> parent;
+    vector<int> rank;
+    public:
+    UnionFind(vector<vector<char>>& grid)
+    {
+        count = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        for(int i = 0; i < m; i ++)
         {
-            for(int j = 0; j < m; j ++)
+            for(int j = 0; j < n; j ++)
             {
                 if(grid[i][j] == '1')
                 {
-                    ans ++;
-                    queue<pair<int, int>> q;
-                    grid[i][j] = '0';
-                    q.push({i, j});
-                    while(!q.empty())
-                    {
-                        int x, y;
-                        tie(x, y) = q.front();
-                        //cout << x << " " << y << " " << ans << endl;
-                        q.pop();
-                        if(x + 1 < n && grid[x + 1][y] == '1') 
-                        {
-                            q.push({x + 1, y});
-                            grid[x + 1][y] = '0';
-                        }
-                        if(x - 1 >= 0 && grid[x - 1][y] == '1')
-                        {
-                            q.push({x - 1, y});
-                            grid[x - 1][y] = '0';
-                        }
-                        if(y + 1 < m && grid[x][y + 1] == '1') 
-                        {
-                            q.push({x, y + 1});
-                            grid[x][y + 1] = '0';
-                        }
-                        if(y - 1 >= 0 && grid[x][y - 1] == '1')
-                        {
-                            q.push({x, y - 1});
-                            grid[x][y - 1] = '0';
-                        }
-                    }
+                    parent.push_back(i * n + j);
+                    count ++;
+                }
+                else
+                {
+                    parent.push_back(-1);
+                }
+                rank.push_back(0);
+            }
+        }
+    }
+    int find(int i)
+    {
+        if(parent[i] != i)
+        {
+            parent[i] = find(parent[i]);
+        }
+        return parent[i];
+    }
+
+    void unite(int x, int y)
+    {
+        int rootx = find(x);
+        int rooty = find(y);
+        if(rootx == rooty) return;
+        if(rank[rootx] < rank[rooty])
+        {
+            swap(rootx, rooty);
+        }
+        parent[rooty] = rootx;
+        rank[rootx] += rank[rooty]; // 更新当前rootx 大树的数量
+        //if(rank[rootx] == rank[rooty]) rank[rootx] -= 1; //?
+        --count;
+    }
+
+    int getCount()
+    {
+        return count;
+    }
+};
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int nr = grid.size();
+        if (!nr) return 0;
+        int nc = grid[0].size();
+
+        UnionFind uf(grid);
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    grid[r][c] = '0';
+                    if (r - 1 >= 0 && grid[r-1][c] == '1') uf.unite(r * nc + c, (r-1) * nc + c);
+                    if (r + 1 < nr && grid[r+1][c] == '1') uf.unite(r * nc + c, (r+1) * nc + c);
+                    if (c - 1 >= 0 && grid[r][c-1] == '1') uf.unite(r * nc + c, r * nc + c - 1);
+                    if (c + 1 < nc && grid[r][c+1] == '1') uf.unite(r * nc + c, r * nc + c + 1);
                 }
             }
         }
-        return ans;
-        
+
+        return uf.getCount();
     }
 };
 // @lc code=end
